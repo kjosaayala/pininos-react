d645bb2803b641d4d6c094e0a4d6d974c8a56f9f
kensy.ayala <kensy.ayala1994@gmail.com>
2021-09-24 18:47:57 -0600

    list helpers

---
+++ src/Arts/artsSettings.jsx
@@ -0,0 +1,51 @@
+import art1 from '../assets/Arte1.PNG';
+import art2 from '../assets/Arte2.PNG';
+import art3 from '../assets/Arte3.PNG';
+import art4 from '../assets/Arte4.PNG';
+import art5 from '../assets/Arte5.PNG';
+import art6 from '../assets/Arte6.PNG';
+import art7 from '../assets/Arte7.PNG';
+import art8 from '../assets/Arte8.PNG';
+
+export const artsData = [
+  {
+    id: 1,
+    painter: 'Leonardo Davinci',
+    image: art1,
+  },
+  {
+    id: 2,
+    painter: 'Leonardo Davinci',
+    image: art2,
+  },
+  {
+    id: 3,
+    painter: 'Leonardo Davinci',
+    image: art3,
+  },
+  {
+    id: 4,
+    painter: 'Leonardo Davinci',
+    image: art4,
+  },
+  {
+    id: 5,
+    painter: 'Leonardo Davinci',
+    image: art5,
+  },
+  {
+    id: 6,
+    painter: 'Leonardo Davinci',
+    image: art6,
+  },
+  {
+    id: 7,
+    painter: 'Leonardo Davinci',
+    image: art7,
+  },
+  {
+    id: 8,
+    painter: 'Leonardo Davinci',
+    image: art8,
+  },
+];
